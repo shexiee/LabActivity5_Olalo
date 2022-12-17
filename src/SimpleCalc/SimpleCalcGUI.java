829c53744404fa39c32ef6f71d8bfc1c5d892655
olalo <olalo.sherlyn49@gmail.com>
2022-12-17 23:06:47 +0800

    Complete SimpleCalc

--- src/SimpleCalc/SimpleCalcGUI.java
+++ src/SimpleCalc/SimpleCalcGUI.java
@@ -3,6 +3,8 @@ package SimpleCalc;
 import LeapYear.LeapYearGUI;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class SimpleCalcGUI extends JFrame {
     private JPanel panel1;
@@ -18,12 +20,39 @@ public class SimpleCalcGUI extends JFrame {
         window.setVisible(true);
         window.setLocationRelativeTo(null);
     }
-
     public SimpleCalcGUI(String title) {
         super(title);
         this.setContentPane(panel1);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.pack();
         lblResult.setEditable(false);
+
+        btnCompute.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                    double num1 = Double.parseDouble(tfNumber1.getText());
+                    double num2 = Double.parseDouble(tfNumber2.getText());
+                    double result = 0;
+                    String op = cbOperations.getSelectedItem().toString();
+                    switch (op) {
+                        case "+":
+                            result = num1 + num2;
+                            lblResult.setText(String.valueOf(String.format("%.0f", result)));
+                            break;
+                        case "-":
+                            result = num1 - num2;
+                            lblResult.setText(String.valueOf(String.format("%.0f", result)));
+                            break;
+                        case "*":
+                            result = num1 * num2;
+                            lblResult.setText(String.valueOf(String.format("%.2f", result)));
+                            break;
+                        case "/":
+                            result = num1 / num2;
+                            lblResult.setText(String.valueOf(String.format("%.2f", result)));
+                            break;
+                    }
+            }
+        });
     }
-}
+    }
