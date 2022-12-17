90182b38b86e47a1e6401f66ec95452860d391e1
olalo <olalo.sherlyn49@gmail.com>
2022-12-17 22:23:46 +0800

    Add LeapYear main function

--- src/LeapYear/LeapYearGUI.java
+++ src/LeapYear/LeapYearGUI.java
@@ -9,21 +9,28 @@ public class LeapYearGUI extends JFrame {
     private JTextField tfYear;
     private JButton btnCheckYear;
 
-    public LeapYearGUI() {
+    public LeapYearGUI(String title) {
         btnCheckYear.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                int year = 0;
+                year = Integer.parseInt(tfYear.getText());
+                if (year % 400 == 0) {
+                    JOptionPane.showMessageDialog(null, "Leap year");
+                } else if (year % 100 != 0 && year % 4 == 0) {
+                    JOptionPane.showMessageDialog(null, "Leap year");
+                } else {
+                    JOptionPane.showMessageDialog(null, "Not a leap year");
+                }
             }
         });
     }
 
     public static void main(String[] args) {
-        LeapYearGUI app = new LeapYearGUI();
+        LeapYearGUI app = new LeapYearGUI("Leap Year Checker");
         app.setContentPane(app.panel1);
         app.setSize(500, 300);
         app.setDefaultCloseOperation(EXIT_ON_CLOSE);
         app.setVisible(true);
-        app.setTitle("Leap Year Checker");
     }
 }
